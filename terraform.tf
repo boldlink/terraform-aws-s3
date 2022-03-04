09afb13e9ab13e6dbc6ca5a5faadc6553bc24f4c
patrick <mukumbu.katiwa@boldlink.io>
2022-03-04 10:56:39 +0300

    NOSTORY: Corrected spelling and locked to version 3

--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,12 @@
-
 terraform {
-  required_version = ">= 0.13"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
   }
+}
+
+provider "aws" {
+  region = "eu-west-1"
 }
